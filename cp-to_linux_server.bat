d1924e704bb29c9aeb01f72636a0072caff182a3
Dave <dskura@gmail.com>
2026-03-27 22:39:00 -0400

    things

---
+++ cp-to_linux_server.bat
@@ -0,0 +1,8 @@
+@echo off
+REM
+REM  Dave Skura, 2023
+REM
+
+echo  copying ...
+
+scp -r /dave/git/daves_ai_trader/* ironman@192.168.0.113:/dave/daves_ai_trader
---
+++ linux.bat
@@ -0,0 +1,9 @@
+@echo off
+REM
+REM  Dave Skura, 2026
+REM
+
+echo ssh to linux server
+
+ssh ironman@192.168.0.113
+
