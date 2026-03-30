b647fa09fa6b8edadcd2b85efdba0ee01fefd990
Dave <dskura@gmail.com>
2026-03-30 00:16:42 -0400

    new files

---
+++ pull-updates.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git pull https://daveskura:$TOKEN@github.com/daveskura/daves_ai_trader.git
---
+++ push-updates.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+git push https://$TOKEN@github.com/daveskura/daves_ai_trader.git
